2f36325e0a183a0da97fa3f7068f20f32c21b2f9
Carlos Pajares <c.pajares@hotmail.com>
2021-11-12 19:25:51 +0100

    Created initializeServer function and added in index

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const chalk = require("chalk");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const debug = require("debug")("socialNet:server");
+
+const app = express();
+const initializeServer = (port) =>
+  new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      console.log("Dentro");
+      debug(chalk.yellow(`Server on, lisen at ${port}`));
+      resolve(server);
+    });
+    server.on("error", () => {
+      debug(chalk.red("Error tring to conect the server"));
+      reject();
+    });
+  });
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+
+module.exports = initializeServer;
