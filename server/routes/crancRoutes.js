ec1f257f48f2bb2bbf522b26bdec2c53f1a5ee12
Carlos Pajares <c.pajares@hotmail.com>
2021-11-13 18:47:00 +0100

    created addFriend function included at crancRoutes

--- server/routes/crancRoutes.js
+++ server/routes/crancRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const getUsers = require("../controller/crancController");
+const { getUsers, addFriend } = require("../controller/crancController");
 const author = require("../middleware/auth");
 
 const router = express.Router();
 
 router.get("/", author, getUsers);
+router.post("/friend", author, addFriend);
 
 module.exports = router;
